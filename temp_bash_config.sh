44b560ffa641d3a05d570680b4bf2eda8923ee24
Christopher Brass <christopher.brass871@gmail.com>
2025-05-29 18:33:25 -0500

    temp bash

--- temp_bash_config.sh
+++ temp_bash_config.sh
@@ -6,3 +6,6 @@ MY_TEST_ENV="asdfASDF"
 printf "${RED}This\n"
 printf "is\n"
 printf "temporary${RESET}\n"
+
+printf "Check permanence with MY_TEST_ENV. This script sets it to asdfASDF"
+printf "Usage: source <(curl https://raw.githubusercontent.com/christopherbrass/dotfiles/refs/heads/main/temp_bash_config.sh)"
